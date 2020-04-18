584d3a06ee082e045690f3204c8a346076e34319
nrojas1 <49444571+nrojas1@users.noreply.github.com>
2020-04-18 20:47:45 +0200

    pylint

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
-import random; import time
+""" The famous dice game in command line format using classes """
+import random; import time; import sys
 
 class Game:
-    """docstring for Game."""
+    """ Game class"""
 
     def __init__(self, n_players):
         self.n_players = n_players
@@ -12,15 +13,16 @@ class Game:
         self.first_turn = True
 
     def round(self):
+        """ Runs a game round where each player has a go """
         for player in self.players:
             print(f"It is now {player.name}'s turn")
             player.throw(6)
-            while player.valid_throw(player.hand) or len(player.hand) == 0:
+            while player.valid_throw(player) or len(player.hand) == 0:
                 if len(player.hand) == 0: # NOT WORKING
                     player.throw(6)
                 # TODO: FIND A SOLUTION FOR WHEN HAND IN EMPTY
-                print(player.hand,' hand')
-                print(player.dice_kept,' dicekept')
+                print(player.hand, ' hand')
+                print(player.dice_kept, ' dicekept')
                 # user input
                 selected_dice = input('what dices do you wish to keep? (seperate by comma) ')
 
@@ -36,10 +38,11 @@ class Game:
                 player.running_score += player.throw_to_score(input_temp)
                 if player.above_water(self.first_turn):
                     while True:
-                        user_stop = input(f"Would you like to stop with {player.running_score} points?\n1: Yes\n2: No\n")
+                        user_stop = input(f"""Would you like to stop with
+                        {player.running_score} points?\n1: Yes\n2: No\n""")
                         try:
                             user_stop = int(user_stop)
-                            if user_stop not in [1,2]:
+                            if user_stop not in [1, 2]:
                                 continue
                             elif user_stop == 1:
                                 player.update_score(player.running_score)
@@ -62,11 +65,13 @@ class Game:
         print(f"end of round {self.n_round}")
         self.n_round += 1
         self.first_turn = False # nah this is a Player attr
-        # TODO: ADD FIRST TURN AS A PLAYER ATTR AND MAKE A FUNCTION THAT CHANGES THE VALUE OF THE THING. MAYBE USE 0 AND 1 INSTEAD OF TRUE AND FALSE
+        # TODO: ADD FIRST TURN AS A PLAYER ATTR AND MAKE A FUNCTION THAT CHANGES
+        # THE VALUE OF THE THING. MAYBE USE 0 AND 1 INSTEAD OF TRUE AND FALSE
         self.update_scoreboard(self.scoreboard)
         print(f"Scoreboard: {self.scoreboard}")
 
     def update_scoreboard(self, scoreboard):
+        """ Creates an instance score and appends it to game scoreboard """
         current_score = dict()
         for player in self.players:
             current_score[player.name] = player.score
@@ -74,8 +79,9 @@ class Game:
         return scoreboard
 
     def podium(self):
+        """ Prints end of game stats """
         finish_line = self.scoreboard[-1]
-        finish_line = sorted((score, name) for (name,score) in finish_line.items())
+        finish_line = sorted((score, name) for (name, score) in finish_line.items())
         winner = finish_line[-1]
         second = finish_line[-2]
         third = finish_line[-3]
@@ -94,29 +100,31 @@ class Game:
     #     return self.top_score
 
 class Player:
-    """Player"""
+    """ Player class """
 
     def __init__(self, name):
         self.name = name
-        self.id = 0
         self.score = 0
         self.running_score = 0
         self.hand = list()
         self.dice_kept = list()
 
-    def update_score(self, x):
-        self.score += x
+    def update_score(self, val):
+        """ add to score """
+        self.score += val
 
     def throw(self, n_dice):
-        self.hand = [random.randint(1,6) for dice in range(0,n_dice)]
+        """ create a Player.hand given the number of dice """
+        self.hand = [random.randint(1, 6) for dice in range(0, n_dice)]
         return self.hand
 
-    def valid_throw(self, hand):
+    def valid_throw(self):
+        """ verifies if Player.hand has playable numbers """
         # looking for triples
-        trpl_dices = [dice for dice in self.hand if self.hand.count(dice) in [3,4,5,6]]
+        trpl_dices = [dice for dice in self.hand if self.hand.count(dice) in [3, 4, 5, 6]]
 
         # looking for golden dice
-        gold_dices = [dice for dice in self.hand if dice in [5,1]]
+        gold_dices = [dice for dice in self.hand if dice in [5, 1]]
 
         # looking for 123456
         one_to_six = (1, 2, 3, 4, 5, 6)
@@ -125,11 +133,16 @@ class Player:
         return True if trpl_dices or gold_dices or diamond_dices else False
 
     def above_water(self, first_turn):
+        """
+            Test if the player has enough points to save them as well as
+            checking if it is the first turn
+        """
         return True if (first_turn and self.running_score >= 450) or (not first_turn and self.running_score >= 350) else False
 
     def throw_to_score(self, hand):
+        """ Takes throw and returns its values as a game score """
         if not hand:
-            exit(0)
+            sys.exit()
         hand.sort(); triple = 3; prev = -1; count = result = 0
 
         # single score
@@ -160,6 +173,7 @@ class Player:
     #     return self.name
 
 def main():
+    """ Main game funciton """
     print("\nHello and welcome to the Dice Game!\n\n~\n\n")
     while True:
         n_players = input("Please enter a number of players or press q to exit ")
@@ -168,7 +182,7 @@ def main():
             break
         try:
             n_players = int(n_players)
-            if n_players not in range(2,7):
+            if n_players not in range(2, 7):
                 continue
         except ValueError:
             continue
@@ -187,7 +201,7 @@ def main():
             dice_game.round()
         final_result = dice_game.podium()
         print(final_result)
-        time.sleep(3); print('\n\nEnd of game');
+        time.sleep(3); print('\n\nEnd of game')
         time.sleep(1); print("\n\n\nAnother one?"); time.sleep(3)
 
 if __name__ == '__main__':
