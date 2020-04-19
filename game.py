ba8527ccf3dd493880e0bbd4b9e81821b73ce824
nrojas1 <49444571+nrojas1@users.noreply.github.com>
2020-04-19 22:22:23 +0200

    Finishing TODO's and fixing bugs that came with it

--- game.py
+++ game.py
@@ -10,64 +10,26 @@ class Game:
         self.top_score = 0
         self.scoreboard = list()
         self.players = list()
-        self.first_turn = True
 
     def round(self):
         """ Runs a game round where each player has a go """
         for player in self.players:
-            print(f"It is now {player.name}'s turn")
+            print(f"\nIt is now {player.name}'s turn")
             player.throw(6)
-            while player.valid_throw() or len(player.hand) == 0:
-                if len(player.hand) == 0: # NOT WORKING
-                    player.throw(6)
-                # TODO: FIND A SOLUTION FOR WHEN HAND IN EMPTY
-                print(player.hand, ' hand')
-                print(player.dice_kept, ' dicekept')
-                # user input
-                selected_dice = input('what dices do you wish to keep? (seperate by comma) ')
-
-                # treat into int
-                str_array = selected_dice.strip().split(',')
-                crt = 0
-                input_temp = list()
-                for d in str_array:
-                    input_temp.append(int(d))
-                    player.dice_kept.append(int(d))
-                    player.hand.pop(player.hand.index(int(d)))
-                    crt += 1
-                player.running_score += player.throw_to_score(input_temp)
-                if player.above_water(self.first_turn):
-                    while True:
-                        user_stop = input(f"""Would you like to stop with {player.running_score} points?\n1: Yes\n2: No\n""")
-                        try:
-                            user_stop = int(user_stop)
-                            if user_stop not in [1, 2]:
-                                continue
-                            elif user_stop == 1: # TODO: YEA this needs fixing...
-                                player.update_score(player.running_score)
-                                break
-                            elif user_stop == 2:
-                                break
-                        except ValueError:
-                            continue
-                    break
-
-                player.throw(len(player.hand))
-            else: #player.valid_throw(player.hand):
-                print(f"{player.hand}\nOut of luck, {player.name} (you bitch)")
+            player.play_round(self)
+            print(f"{player.hand}\nEnd of {player.name}'s turn... you bitch\n")
+            time.sleep(2)
             player.running_score = 0
-            print(player.running_score)
-
         [player.dice_kept.clear() for player in self.players]
-
-
         print(f"end of round {self.n_round}")
         self.n_round += 1
-        self.first_turn = False # nah this is a Player attr
-        # TODO: ADD FIRST TURN AS A PLAYER ATTR AND MAKE A FUNCTION THAT CHANGES
-        # THE VALUE OF THE THING. MAYBE USE 0 AND 1 INSTEAD OF TRUE AND FALSE
+        if self.n_round > 2:
+            self.topscore()
         self.update_scoreboard(self.scoreboard)
         print(f"Scoreboard: {self.scoreboard}")
+        time.sleep(3)
+        if self.top_score <= 10000:
+            print('Starting next round...'); time.sleep(2)
 
     def update_scoreboard(self, scoreboard):
         """ Creates an instance score and appends it to game scoreboard """
@@ -91,12 +53,16 @@ class Game:
         string += f"SECOND: {second[1]} with {second[0]} points\n\n"
         string += f"THIRD: {third[1]} with {third[0]} points\n"
         return string
+
     # def get_score(self):
     #     return self.score
     #
-    # def get_topscore(self, scores):
-    #     self.top_score = max(scores)
-    #     return self.top_score
+    def topscore(self):
+        latest_round_score = self.scoreboard[-1]
+        latest_round_score = sorted((score, name) for (name, score) in latest_round_score.items())
+        top = latest_round_score[-1][0]
+        self.top_score = top
+        return self.top_score
 
 class Player:
     """ Player class """
@@ -107,18 +73,20 @@ class Player:
         self.running_score = 0
         self.hand = list()
         self.dice_kept = list()
+        self.stop_round = 0
+        self.is_on_scoreboard = 0
 
     def update_score(self, val):
-        """ add to score """
+        """ Add Player.running_score to Player.score """
         self.score += val
 
     def throw(self, n_dice):
-        """ create a Player.hand given the number of dice """
+        """ Creates a Player.hand given the number of dice """
         self.hand = [random.randint(1, 6) for dice in range(0, n_dice)]
         return self.hand
 
     def valid_throw(self):
-        """ verifies if Player.hand has playable numbers """
+        """ Verifies if Player.hand has playable numbers """
         # looking for triples
         trpl_dices = [dice for dice in self.hand if self.hand.count(dice) in [3, 4, 5, 6]]
 
@@ -131,15 +99,15 @@ class Player:
 
         return True if trpl_dices or gold_dices or diamond_dices else False
 
-    def above_water(self, first_turn):
+    def above_water(self):
         """
-            Test if the player has enough points to save them as well as
-            checking if it is the first turn
+            Tests if the player has enough points to save them as well as
+            checking if it is his first score on the scoreboard
         """
-        return True if (first_turn and self.running_score >= 450) or (not first_turn and self.running_score >= 350) else False
+        return True if (self.is_on_scoreboard == 0 and self.running_score >= 450) or (self.is_on_scoreboard == 1 and self.running_score >= 350) else False
 
     def throw_to_score(self, hand):
-        """ Takes throw and returns its values as a game score """
+        """ Takes dice values and returns its game score """
         if not hand:
             sys.exit()
         hand.sort(); triple = 3; prev = -1; count = result = 0
@@ -165,11 +133,82 @@ class Player:
                     result += (dice*1000)-300
         return result
 
+    def play_round(self, game):
+        """
+            Game action function for individual player
+        """
+        # play while you have a valid throw or your hand is empty
+        while self.valid_throw() or len(self.hand) == 0:
+            if len(self.hand) == 0:
+                print('cool! A new hand coming right up\n')
+                time.sleep(2)
+                self.throw(6)
+                continue
+            print(self.hand, ' hand')
+            print(self.dice_kept, ' dicekept')
+            while True:
+                # user input
+                selected_dice = input('What dices do you wish to keep? (seperate by comma)\n')
+                try:
+                    dice_arr = selected_dice.strip().split(',')
+                    dice_arr = [int(dice) for dice in dice_arr]
+                    #print(dice_arr,'\n')
+                    hand_match = [dice for dice in dice_arr if dice in self.hand]
+                    #print(len(dice_arr),':',len(hand_match),'\n')
+                    if len(hand_match) != len(dice_arr):
+                        raise ValueNotInError
+                    if not valid_selection(hand_match):
+                        raise ValueNotCorrect
+                    for dice in hand_match:
+                        self.dice_kept.append(int(dice))
+                        self.hand.pop(self.hand.index(int(dice)))
+                    break
+                except ValueError:
+                    print('The dice entered must be numbers\n')
+                except ValueNotInError:
+                    print("The dice don't match the hand... try again\n")
+                except ValueNotCorrect:
+                    print('Ones, fives and triples are only allowed in selection\n')
+            self.running_score += self.throw_to_score(hand_match)
+            if self.above_water() and len(self.hand) != 0:
+                while True:
+                    user_stop = input(f"""\nWould you like to stop with {self.running_score} points?\n1: Yes\n2: No\n""")
+                    try:
+                        user_stop = int(user_stop)
+                        if user_stop not in [1, 2]:
+                            continue
+                    except ValueError:
+                        print('1: Continue\n2: Stop here')
+                        continue
+                    if user_stop == 1:
+                        self.update_score(self.running_score)
+                        self.is_on_scoreboard = 1
+                        break
+
+                    elif user_stop == 2:
+                        self.throw(len(self.hand))
+                        self.stop_round = 1
+                        break
+            else:
+                print('replaying...')
+                self.throw(len(self.hand))
     # def get_score(self):
     #     return self.score
     #
     # def get_name(self):
     #     return self.name
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class ValueNotInError(Error):
+    """Raised when the user input's values don't match the Game hand values"""
+    pass
+
+class ValueNotCorrect(Error):
+    """Raised when user's input is not a Game scoring dice"""
+    pass
 
 def main():
     """ Main game funciton """
@@ -196,12 +235,29 @@ def main():
             dice_game.players.append(player)
 
         # simple game loop
-        while dice_game.n_round < 3: # TODO: SET A CONDITION WHICH STOPS AT LIMIT SCORE
+        while dice_game.top_score <= 500:
             dice_game.round()
         final_result = dice_game.podium()
         print(final_result)
         time.sleep(3); print('\n\nEnd of game')
         time.sleep(1); print("\n\n\nAnother one?"); time.sleep(3)
+
+
+def valid_selection(list):
+    """ Verifies if Player.hand has playable numbers """
+    # looking for triples
+    trpl_dices = [dice for dice in list if list.count(dice) in [3, 4, 5, 6]]
+
+    # looking for golden dice
+    gold_dices = [dice for dice in list if dice in [5, 1]]
+
+    # looking for 123456
+    one_to_six = (1, 2, 3, 4, 5, 6)
+    diamond_dices = all(elem in list for elem in one_to_six)
+
+    return True if trpl_dices or gold_dices or diamond_dices else False
+
+
 
 if __name__ == '__main__':
     main()
